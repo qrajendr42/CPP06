8374176d71ad04c665829988f6e1e72d00ed2fb7
Qhaby <qhabylen@gmail.com>
2026-04-29 21:15:51 +0800

    hihi

--- ex02/identify.cpp
+++ ex02/identify.cpp
@@ -14,39 +14,26 @@ Base* generate(void)
     return new C();
 }
 
-void identify(Base* p)
+void    identify(Base *p)
 {
     if (dynamic_cast<A*>(p))
-        return (void)(std::cout << "A" << std::endl);
-    if (dynamic_cast<B*>(p))
-        return (void)(std::cout << "B" << std::endl);
-    if (dynamic_cast<C*>(p))
-        return (void)(std::cout << "C" << std::endl);
+        std::cout << "A" << std::endl;
+    
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
 }
 
-void identify(Base& p)
+void identify(Base &p)
 {
-    try
-    {
-        (void)dynamic_cast<A&>(p);
+    if (A* a = dynamic_cast<A*>(&p))
         std::cout << "A" << std::endl;
-        return;
-    }
-    catch (const std::exception&) {}
 
-    try
-    {
-        (void)dynamic_cast<B&>(p);
+    else if (B* b = dynamic_cast<B*>(&p))
         std::cout << "B" << std::endl;
-        return;
-    }
-    catch (const std::exception&) {}
-
-    try
-    {
-        (void)dynamic_cast<C&>(p);
+    
+    else if (C* c = dynamic_cast<C*>(&p))
         std::cout << "C" << std::endl;
-        return;
-    }
-    catch (const std::exception&) {}
 }
